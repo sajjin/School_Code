aca88024892ce6a1405cb687b165ae85c0fec1bc
sajjin <sajjinnijjar@gmail.com>
2023-09-19 11:23:43 -0700

    working version

--- COMP 7005/Assignment1/client.py
+++ COMP 7005/Assignment1/client.py
@@ -1,56 +1,73 @@
 import socket
 import sys
 import os
+import tqdm
 import time
 
-def connect_send(BUFFER_SIZE, socket_path, SEPARATOR):
-    # Create a Unix domain socket client
-    client_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+# Define the server address (host and port)
+server_host = "127.0.0.1"
+server_port = 12345
 
+def send_files(SEPARATOR, file_names, client_socket):
     try:
-        # Connect to the server
-        client_socket.connect(socket_path)
-
-        # Get file names from command-line arguments
-        file_names = sys.argv[1:]
-
 
         for file_name in file_names:
-            # Send the file name to the server
-            client_socket.sendall(file_name.encode('utf-8') + "|".encode('utf-8'))
 
-            # Send the size of the file to the server
+
             file_size = os.path.getsize(file_name)
-            client_socket.sendall(str(file_size).encode('utf-8') + "|".encode('utf-8'))
+            client_socket.send(f"{file_name}{SEPARATOR}{file_size}".encode())
+
 
             # Open the file and send its contents to the server
+            progress = tqdm.tqdm(range(file_size), f"Sending {file_name}", unit="B", unit_scale=True, unit_divisor=1024)
             with open(file_name, 'rb') as file:
                 while True:
-                    data = file.read(BUFFER_SIZE)
+ 
+                    data = file.read(file_size)
                     if not data:
                         break
-                    client_socket.sendall(data)
-                    break
+                    client_socket.send(data)
+                    progress.update(len(data))
+                    if file_size < 4096:
+                        time.sleep(1)
+                    elif file_size >= 4096:
+                        time.sleep(2)
+
             
-            client_socket.sendall(SEPARATOR.encode('utf-8'))
-
-
     except FileNotFoundError:
         print("File not found.")
     except Exception as e:
         print(f"Error: {e}")
     finally:
         # Clean up the client socket
-        time.sleep(1)
         client_socket.close()
+            
 
+
+# Create a Unix domain socket client
 
 def main():
     # Define the path to the Unix domain socket
     socket_path = "/tmp/my_unix_socket"
-    SEPARATOR = "<>"
-    BUFFER_SIZE = 1024 * 100
-    connect_send(BUFFER_SIZE, socket_path, SEPARATOR)
+    SEPARATOR = "|"
+    BUFFER_SIZE = 4096
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        # Connect to the server
+        client_socket.connect((server_host, server_port))
+
+        # Get file names from command-line arguments
+        file_names = sys.argv[1:]
+        # file_names = ["/home/sajjin/Documents/School_Code/COMP 7005/Assignment1/is.txt", "/home/sajjin/Documents/School_Code/COMP 7005/Assignment1/a.png"]
+
+        #  Send multiple files to the server
+        send_files( SEPARATOR, file_names, client_socket)
+    except ConnectionRefusedError:
+        print("Connection to the server failed.")
+    finally:
+        # Clean up the client socket
+        client_socket.close()
 
 
 if __name__ == '__main__':
--- COMP 7005/Assignment1/server.py
+++ COMP 7005/Assignment1/server.py
@@ -1,105 +1,118 @@
 import socket
 import os
+import tqdm
+
+# Define the size of the buffer
+BUFFER_SIZE = 1024
+
+SEPARATOR = "|"
+
+# Define the path to the Unix domain socket
+socket_path = "/tmp/my_unix_socket"
+
+# Define the server address (host and port)
+server_host = "127.0.0.1"
+server_port = 12345
+
+# Define the directory where files will be stored
+storage_directory = "server_storage"
+
+# Create the storage directory if it doesn't exist
+if not os.path.exists(os.path.join(os.path.dirname(__file__),storage_directory)):
+    os.makedirs(os.path.join(os.path.dirname(__file__),storage_directory))
 
 
-def connect_recive(BUFFER_SIZE, socket_path, storage_directory):
+def receive_files(connection):
+
     try:
-        # Create the storage directory if it doesn't exist
-        if not os.path.exists(os.path.join(os.path.dirname(__file__),storage_directory)):
-            os.makedirs(os.path.join(os.path.dirname(__file__),storage_directory))
 
-        # Create a Unix domain socket server
-        server_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        # Receive and store files from the client
+        while True:
+            # Receive the file name from the client
+            try:
+                received = connection.recv(BUFFER_SIZE).decode()
+            except Exception as e:
+                received = connection.recv(BUFFER_SIZE)
+            if not received:
+                break
+            file_name, file_size = received.split(SEPARATOR)
+            # remove absolute path if there is
+            file_name = os.path.basename(file_name)
+            # convert to integer
+            file_size = int(file_size)
 
-        # Bind the socket to the defined path
-        server_socket.bind(socket_path)
 
-        # Listen for incoming connections
-        server_socket.listen(1)
-        print(f"Server is listening on {socket_path}")
+            if not file_name:
+                break
 
-        try:
+
+
+
+            # Check for duplicate file names and handle as desired
+            original_file_name = file_name
+            file_counter = 1
             while True:
-                # Accept incoming connections
-                connection, client_address = server_socket.accept()
-                print(f"Accepted connection from {client_address}")
-
-                try:
-                    # Receive and store files from the client
-                    while True:
-                        # Receive the file name from the client
-                        file_name = connection.recv(BUFFER_SIZE).decode('utf-8')
-                        if file_name == "":
-                            break
-                        for sec_file in file_name.split("<>"):
-
-                            file_detail = sec_file.split("|")
-                
-                            try:
-                                file_name = file_detail[0]
-                                file_size = int(file_detail[1])
-                            except:
-                                break
+                file_path = os.path.join(os.path.dirname(__file__), storage_directory, file_name)
+                if not os.path.exists(file_path):
+                    break
+                base_name, extension = os.path.splitext(original_file_name)
+                file_name = f"{base_name}_{file_counter}{extension}"
+                file_counter += 1
+            file_path = os.path.join(os.path.dirname(__file__), storage_directory, file_name)
 
 
-                            if not file_name:
-                                break
-
-                            # Check for duplicate file names and handle as desired
-                            original_file_name = file_name
-                            file_counter = 1
-                            while True:
-                                file_path = os.path.join(os.path.dirname(__file__), storage_directory, file_name)
-                                if not os.path.exists(file_path):
-                                    break
-                                base_name, extension = os.path.splitext(original_file_name)
-                                file_name = f"{base_name}_{file_counter}{extension}"
-                                file_counter += 1
-                            file_path = os.path.join(os.path.dirname(__file__), storage_directory, file_name)
+            # Receive the size of the file from the client
+            print(f"Receiving file: {file_name} ({file_size} bytes)")
 
 
-                            # Receive the size of the file from the client
-                            print(f"Receiving file: {file_name} ({file_size} bytes)")
+            # Receive and save the file data
+            progress = tqdm.tqdm(range(file_size), f"Receiving {file_name}", unit="B", unit_scale=True, unit_divisor=1024)
+            with open(file_path, 'wb') as file:
+                received_data = 0
+                while received_data < file_size:
+                    data = connection.recv(file_size)
+                    if not data:
+                        break
+                    file.write(data)
+                    received_data += len(data)
+                    progress.update(len(data))
+                print(f"Received and saved file: {file_name}")
 
 
-                            # Receive and save the file data
-                            with open(file_path, 'wb') as file:
-                                received_data = 0
-                                while received_data < file_size:
-                                    data = file_detail[2].encode('utf-8')
-                                    if not data:
-                                        break
-                                    file.write(data)
-                                    received_data += len(data)
-                                print(f"Received and saved file: {file_name}")
-
-                finally:
-                    # Clean up the connection
-                    connection.close()
-
-        except KeyboardInterrupt:
-            # Handle Ctrl-C to gracefully exit the server
-            print("Server is shutting down.")
-            server_socket.close()
-            os.unlink(socket_path)
+    except KeyboardInterrupt:
+        # Handle Ctrl-C to gracefully exit the server
+        print("Server is shutting down.")
     except Exception as e:
         print(f"Error: {e}")
-        server_socket.close()
-        os.unlink(socket_path)
-
+    finally:
+        # Clean up the connection
+        connection.close()
 
 
 
 def main():
-    BUFFER_SIZE = 1024 * 100
+    # Create a TCP socket server
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # Define the path to the Unix domain socket
-    socket_path = "/tmp/my_unix_socket"
+    # Bind the socket to the server address
+    server_socket.bind((server_host, server_port))
 
-    # Define the directory where files will be stored
-    storage_directory = "server_storage"
+    # Listen for incoming connections
+    server_socket.listen(1)
 
-    connect_recive(BUFFER_SIZE, socket_path, storage_directory)
+    try:
+        while True:
+            # Accept incoming connection from the client
+            client_socket, client_address = server_socket.accept()
+            print(f"Accepted connection from {client_address}")
+
+            # Receive and save files from the client
+            receive_files(client_socket)
+
+    except KeyboardInterrupt:
+        # Handle Ctrl-C to gracefully exit the server
+        print("Server is shutting down.")
+        server_socket.close()
 
 
 
